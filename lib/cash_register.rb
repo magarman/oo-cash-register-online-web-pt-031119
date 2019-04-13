ca23a058b9016da05fc35de86ad069f7d5260e64
Mari Garman <garman.mariana@gmail.com>
2019-04-13 17:06:48 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -12,7 +12,6 @@ class CashRegister
   
   def add_item(title, price, quantity=1)
     self.total += (price * quantity)
-    self.last_trans = self.total
     #quantity.times do
      # @items << title
       #quantity -= 1
